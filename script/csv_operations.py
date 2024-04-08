7164ccf641d960b42a345b493f87de1eb4642388
Robin Bergvall <robinbergvall19@gmail.com>
2024-04-08 17:08:17 +0200

    statistic files now saved in statistics folder

--- script/csv_operations.py
+++ script/csv_operations.py
@@ -1,18 +1,20 @@
 import csv
 
+statistics_folder = "statistics/"
+
 # Generates a csv file, provides statistics of the unit test generation.
 def generate_csv_file(file_name):
-    csv_filename = f"{file_name}_stats.csv"
+    csv_filename = f"{statistics_folder}{file_name}_stats.csv"
     with open(csv_filename, 'w', newline='') as csvfile:
-        fieldnames = ['File Name', 'Number of Tries', 'Pass Status', '% Stmts', '% Branch', '% Funcs', '% Lines', 'Temperature']
+        fieldnames = ['File Name', 'Number of Tries', 'Pass Status', 'Stmts', 'Branch', 'Funcs', 'Lines', 'Temperature']
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         writer.writeheader()
 
 # Add a unit test generation to the statistics file.
 def append_to_csv_files(file_name, number_of_tries, pass_status, temperature, coverage, test_file_name):
-    csv_filename = f"{file_name}_stats.csv"
+    csv_filename = f"{statistics_folder}{file_name}_stats.csv"
     with open(csv_filename, 'a', newline='') as csvfile:
-        fieldnames = ['File Name', 'Number of Tries', 'Pass Status', '% Stmts', '% Branch', '% Funcs', '% Lines', 'Temperature']
+        fieldnames = ['File Name', 'Number of Tries', 'Pass Status', 'Stmts', 'Branch', 'Funcs', 'Lines', 'Temperature']
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         writer.writerow({
             'File Name': f"{test_file_name}",
