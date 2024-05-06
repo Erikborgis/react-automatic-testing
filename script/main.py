dfd765d91da80ed2a0c75680506f99393faf5909
Erik Borgström <erik.borgstrom@softwerk.se>
2024-05-06 10:49:38 +0200

    Fixed semaphore
    - Fixed multithreading so we dont send to many requests at the same time.

--- script/main.py
+++ script/main.py
@@ -9,57 +9,61 @@ import gpt_prompts
 import os
 import threading
 
+semaphore = threading.Semaphore(15)
 
 def process_react_component(file_name, path):
 
-    max_number_of_tries = 3 # Specify how many retries the test generation should make if test fails Must be more than one.
-    temperature = [0.2, 0.4, 0.6, 0.8, 1]
-    reruns = 3 # Specifies how many times a certain file should be rerun
+    try:
+        max_number_of_tries = 2 # Specify how many retries the test generation should make if test fails. Must be more than one.
+        temperature = [0.2]
+        reruns = 1 # Specifies how many times a certain file should be rerun
 
-    react_component_text = file_operations.read_file(path)
-           
-    if react_component_text:
-        csv_operations.generate_csv_file(file_name)
-        for rerun in range(reruns):
-            for temp in temperature:                   
-                test_file_name = f"{file_name}_{rerun}_{temp}.test.js"
-                
-                gpt_message = gpt_prompts.first_prompt(react_component_text, path)
-                test_content = write_to_gpt.call_openai_api(gpt_message, temp)
+        react_component_text = file_operations.read_file(path)
+            
+        if react_component_text:
+            csv_operations.generate_csv_file(file_name)
+            for rerun in range(reruns):
+                for temp in temperature:                   
+                    test_file_name = f"{file_name}_{rerun}_{temp}.test.js"
+                    
+                    gpt_message = gpt_prompts.first_prompt(react_component_text, path)
+                    test_content = write_to_gpt.call_openai_api(gpt_message, temp)
 
-                # Generate test file and check if the test runs correctly
-                file_operations.create_test_file(test_content, test_file_name)
-                test_status, test_log = test_handler.run_test(test_file_name)
+                    # Generate test file and check if the test runs correctly
+                    file_operations.create_test_file(test_content, test_file_name)
+                    test_status, test_log = test_handler.run_test(test_file_name)
 
-                # Regenerates unit tests until the tests pass. Max 5 tries.
-                if test_status:
-                    number_of_tries = 1
-                else:
-                    created_test_file = react_component_text = file_operations.read_file(f"./tests/{test_file_name}")
-                    test_files = [created_test_file]
-                    test_errors = [test_log]
-                    for number_of_tries in range(2, max_number_of_tries + 1):
-                        
-                        assistant_prompts = gpt_prompts.create_assistant_prompts(test_files, test_errors)
+                    # Regenerates unit tests until the tests pass. Max 5 tries.
+                    if test_status:
+                        number_of_tries = 1
+                    else:
+                        created_test_file = react_component_text = file_operations.read_file(f"./tests/{test_file_name}")
+                        test_files = [created_test_file]
+                        test_errors = [test_log]
+                        for number_of_tries in range(2, max_number_of_tries + 1):
+                            
+                            assistant_prompts = gpt_prompts.create_assistant_prompts(test_files, test_errors)
 
-                        gpt_message = gpt_prompts.retry_prompt(react_component_text, path, assistant_prompts)
+                            gpt_message = gpt_prompts.retry_prompt(react_component_text, path, assistant_prompts)
 
-                        test_content = write_to_gpt.regenerate_test(gpt_message, temp)
+                            test_content = write_to_gpt.regenerate_test(gpt_message, temp)
 
-                        # Generate test file and check if the test runs correctly
-                        file_operations.create_test_file(test_content, test_file_name)
-                        test_status, test_log = test_handler.run_test(test_file_name)
-                        if test_status:
-                            break
-                        else:
-                            created_test_file = react_component_text = file_operations.read_file(f"tests/{test_file_name}")
-                            test_files.append(created_test_file)
-                            test_errors.append(test_log)
+                            # Generate test file and check if the test runs correctly
+                            file_operations.create_test_file(test_content, test_file_name)
+                            test_status, test_log = test_handler.run_test(test_file_name)
+                            if test_status:
+                                break
+                            else:
+                                created_test_file = react_component_text = file_operations.read_file(f"tests/{test_file_name}")
+                                test_files.append(created_test_file)
+                                test_errors.append(test_log)
 
-                # Check the code coverage on the react component the test is responsible for.
-                coverage = code_coverage.check_coverage_specific_test(test_file_name)
-                # Add parameters to create statistics file.
-                csv_operations.append_to_csv_files(file_name, number_of_tries, test_status, temp, coverage, test_file_name) 
+                    # Check the code coverage on the react component the test is responsible for.
+                    coverage = code_coverage.check_coverage_specific_test(test_file_name)
+                    # Add parameters to create statistics file.
+                    csv_operations.append_to_csv_files(file_name, number_of_tries, test_status, temp, coverage, test_file_name) 
+    finally:
+        semaphore.release()
 
 
 def main():
@@ -74,6 +78,8 @@ def main():
         thread = threading.Thread(target=process_react_component, args=(file_name, path))
         threads.append(thread)
         thread.start()
+
+        semaphore.acquire()
         
     for thread in threads:
         thread.join()
